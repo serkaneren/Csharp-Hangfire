9cf06ce8eda2e9df696cc420bfa41bf7395950c8
serkaneren <serkaneren@outlook.com>
2019-04-08 17:14:13 +0300

    Add project files.

---
+++ Hangfire/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Hangfire.Startup))]
+
+namespace Hangfire
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage(@"Server=SERKANEREN8A72\SQLEXPRESS;Database=Hangfire;Integrated Security=true");
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
