21c964df66426932b3b0ff209f1f2dc4508cf0be
serkaneren <serkaneren@outlook.com>
2019-04-09 10:38:16 +0300

    NO-ISSUE / Hangfire Configuration ayarları

---
+++ Hangfire/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Hangfire.Startup))]
+
+namespace Hangfire
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Db Bilgilerimizi bu alana giriyoruz.
+            GlobalConfiguration.Configuration.UseSqlServerStorage(@"Server=SİZİN_LOCALHOST;Database=Hangfire;Integrated Security=true");
+            //Dashboardunu aktif ediyoruz. 
+            app.UseHangfireDashboard();
+            //Serverı aktif hale getiriyoruz.
+            app.UseHangfireServer();
+        }
+    }
+}
